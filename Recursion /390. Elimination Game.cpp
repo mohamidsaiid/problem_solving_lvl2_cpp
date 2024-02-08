fe78bebc464807dea4b312e71df27189d4a00d75
mohamidsaiid <100053790+mohamidsaiid@users.noreply.github.com>
2024-02-08 22:12:03 +0200

    Create 390. Elimination Game.cpp

---
+++ Recursion /390. Elimination Game.cpp
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/elimination-game/
+
+class Solution {
+public:
+    int lastRemaining(int n) {
+        if(n == 1) return 1;
+        return 2*(n/2 - lastRemaining(n/2) + 1);
+    }
+};
